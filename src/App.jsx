92fb1cf3f0ff5839ad31b9583e2f285fc78ce88e
Pankaj Tomar <pankaj.tomar.engineer@gmail.com>
2026-09-12 15:31:10 +0530

    image and cv fixed

--- src/App.jsx
+++ src/App.jsx
@@ -260,7 +260,7 @@ export default function App() {
                   variant="outlined"
                   size="large"
                   component="a"
-                  href="../src/assets/images/Pankaj_MERN_6+yr_Lead.pdf"
+                  href="./public/Pankaj_MERN_6+yr_Lead.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
                   endIcon={<DownloadIcon />}
@@ -310,7 +310,7 @@ export default function App() {
               >
                 <Box
                   component="img"
-                  src={"../src/assets/images/pankajPhoto.png"}
+                  src={"./public/pankajPhoto.png"}
                   alt="Pankaj Tomar"
                   sx={{
                     width: "100%",
--- src/utils/icons.jsx
+++
@@ -1,3 +0,0 @@
-export default icons = {
-  profileImage: "../src/assets/images/pankajPhoto.png",
-};
